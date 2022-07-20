764525cd05ddd6c450a9a1784a5c496e208ac3e1
Beatriz <beatriz.mediatto2@gmail.com>
2022-07-20 15:35:59 -0300

    Primeiro exemplo JavaScript

---
+++ Fundamentos/bloco04-introducao-javascript-e-logica-de-programacao/dia01-primeiros-passos/exemplo1.js
@@ -0,0 +1,9 @@
+const myName = "Beatriz";
+const birthCity = "Volta Redonda";
+var birthYear = 2000;
+
+birthYear = 2030;
+birthCity = "Rio de Janeiro";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
