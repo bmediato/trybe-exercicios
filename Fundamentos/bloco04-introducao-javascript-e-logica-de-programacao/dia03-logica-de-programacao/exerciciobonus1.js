f80ea3875079aece92f212d0d47b37986ac8110a
Beatriz <beatriz.mediatto2@gmail.com>
2022-07-23 14:37:33 -0300

    Exercicio bonus

---
+++ Fundamentos/bloco04-introducao-javascript-e-logica-de-programacao/dia03-logica-de-programacao/exerciciobonus1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let symbol = '*';
+let inputline = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex +=1) {
+    inputline = inputline + symbol;
+}
+for (let lineIndex =0; lineIndex <n; lineIndex +=1){
+console.log(inputline)
+}
+
