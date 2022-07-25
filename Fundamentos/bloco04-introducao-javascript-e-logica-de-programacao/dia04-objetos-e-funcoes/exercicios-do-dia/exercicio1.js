4edffaa79d4c5df165325a9a4793e73ad59b6366
Beatriz <beatriz.mediatto2@gmail.com>
2022-07-25 20:03:32 -0300

    Primeiro exercicio finalizado

--- Fundamentos/bloco04-introducao-javascript-e-logica-de-programacao/dia04-objetos-e-funcoes/exercicios-do-dia/exercicio1.js
+++ Fundamentos/bloco04-introducao-javascript-e-logica-de-programacao/dia04-objetos-e-funcoes/exercicios-do-dia/exercicio1.js
@@ -9,5 +9,16 @@ let info = {
   info['recorrente'] = 'Sim';
 
   for (let key in info) {
-    console.log(key)
+    console.log(info[key]);
   }
+
+  let dados = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178 ",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  console.log( info.personagem + ' e ' + dados.personagem);
+  console.log(info.origem + ' e ' + dados.origem);
+  console.log(info.nota + ' e ' + dados.nota);
+  console.log(info.recorrente + ' e ' + dados.recorrente);
