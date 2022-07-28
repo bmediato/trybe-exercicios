2cb36270d20061572ecc65611f2a89e822656a5d
Beatriz <beatriz.mediatto2@gmail.com>
2022-07-28 16:03:34 -0300

    Criando irmao

---
+++ Fundamentos/bloco05-dom-eventos-e-web-storage/dia02-trabalhando-com-elementos/script1.js
@@ -0,0 +1,10 @@
+// 1.Crie um irmão para elementoOndeVoceEsta
+let pai = document.getElementById('pai');
+let irmao = document.createElement('section');
+irmao.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmao); 
+
+// 2.Crie um filho para elementoOndeVoceEsta
+
+//3.Crie um filho para primeiroFilhoDoFilho
+//4.A partir desse filho criado, acesse terceiro filho
