8fedeb25afd02980d52c5c6ab405aba254670ea8
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-05 18:12:55 -0300

    exercicios do dia

--- Fundamentos/bloco06-forms-flexbox-responsivo/dia01-forms/exercicios-do-dia/script.js
+++ Fundamentos/bloco06-forms-flexbox-responsivo/dia01-forms/exercicios-do-dia/script.js
@@ -1,7 +1,14 @@
 const botao = document.getElementById('botao');
+const limpaBotao = document.getElementById('limpa');
 
 botao.addEventListener('click', parouBotao) 
 
 function parouBotao (event) {
     event.preventDefault();
+}
+
+limpaBotao.addEventListener('click', limpaTudo)
+
+function limpaTudo(event) {
+    
 }
