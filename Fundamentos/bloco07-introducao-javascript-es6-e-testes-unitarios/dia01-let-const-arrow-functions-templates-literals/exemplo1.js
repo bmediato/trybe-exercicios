04109dffba0562af625510a13ab66cbb228d5709
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-12 18:01:07 -0300

    exemplos do dia

---
+++ Fundamentos/bloco07-introducao-javascript-es6-e-testes-unitarios/dia01-let-const-arrow-functions-templates-literals/exemplo1.js
@@ -0,0 +1,8 @@
+const printName = () => 'Lucas';
+console.log(printName());
+
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
+
+const multiplication = (number, multi) => number * multi;
+console.log(multiplication(8, 2));
