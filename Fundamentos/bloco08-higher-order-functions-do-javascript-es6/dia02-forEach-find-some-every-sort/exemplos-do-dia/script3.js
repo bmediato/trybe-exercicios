2b7ba7d586dbba1ca4d625eede63609750fb69e2
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-23 06:51:33 -0300

    exemplos de find() e forEach()

---
+++ Fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-forEach-find-some-every-sort/exemplos-do-dia/script3.js
@@ -0,0 +1,9 @@
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((nome) => nome.length >=5);
+}
+
+
+
+console.log(findNameWithFiveLetters());
