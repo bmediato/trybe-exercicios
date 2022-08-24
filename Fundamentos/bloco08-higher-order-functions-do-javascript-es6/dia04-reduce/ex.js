90a7e2ce2c4e0fce2ee6c2f5f4d3bc48127fed95
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-24 14:39:59 -0300

    exemplo do dia - reduce

---
+++ Fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia04-reduce/ex.js
@@ -0,0 +1,8 @@
+const números = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const pares = números.filtro((elemento) => elemento % 2 === 0);
+
+const soma = pares.reduz((acc, curr) => acc + curr);
+
+consola.log(pares);
+consola.log(soma);
