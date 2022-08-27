e5c72cea391f97eef2e1a6a76590d91920f00551
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-27 14:14:15 -0300

    revisando exemplo do dia

--- Fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia01-introducao-a-higher-order-functions/exercicios-do-dia/script2.js
+++
@@ -1,8 +0,0 @@
-const numAleatorio = (numApostado, callback) => {
-  let x = Math.floor((Math.random() * 5) +1);
-  return callback(numApostado, x) ? 'Lucky day, you won!' : 'Try again!';
-}
-
-const verifica = (numApostado, x) => numApostado === x;
-
-console.log(numAleatorio(4, verifica));
