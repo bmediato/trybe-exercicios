3d6f87c38effee689c2f055795a6fb5cf2dde854
Beatriz <beatriz.mediatto2@gmail.com>
2022-09-19 19:02:48 -0300

    exercicio de fixação do dia

---
+++ Desenvolvimento-Front-end/bloco10-introducao-react/dia01-hello-world-no-react/exercicios-do-dia/my-app/src/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class nome extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Beatriz Mediato</h1>
+        <p>Tenho 21 anos e sou de Volta Redonda- RJ.</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Unix e Bash</li>
+          <li>JavaScript</li>
+          <li>Html e CSS</li>
+          <li>HOF</li>
+          <li>FlexBox</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default nome;
