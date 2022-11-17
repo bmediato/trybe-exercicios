70d623cc0e5f7b15168143e6e816e562ca771a82
Beatriz <beatriz.mediatto2@gmail.com>
2022-11-17 19:17:52 -0300

    exercicios do dia

--- Desenvolvimento-Front-end/secao9-Context-Api-Hooks/dia01-userState-e-userEffect/form/src/App.js
+++ Desenvolvimento-Front-end/secao9-Context-Api-Hooks/dia01-userState-e-userEffect/form/src/App.js
@@ -1,43 +1,10 @@
-import React, {userState} from 'react';
+import './App.css';
+import Form from './component/Form';
 
-function Form() {
-  const [name, setName] = userState('');
-  const [age, setAge] = userState('');
-  const [city, setCity] = userState('');
-
+function App() {
   return (
-    <form>
-      <fieldset>
-        <h1>Dados Pessoais</h1>
-        <label htmlFor="name">
-          Nome
-          <input 
-          type="text" 
-          id="name" 
-          value={name} 
-          onChange={({target}) => setName(target.value) }
-          ></input>
-          </label>
-          <label htmlFor="age">
-            Idade
-            <input
-            type="number"
-            id="age"
-            value={age}
-            onChange={({target}) => setAge(target.value)}></input>
-          </label>
-          <label htmlFor="city">
-            Location
-            <input 
-            type="text"
-            id="city"
-            value={city}
-            onChange={ ({target}) => setCity(target.value)}>
-            </input>
-          </label>
-      </fieldset>
-    </form>
-   );
+   <Form />
+  );
 }
 
-export default Form;
+export default App;
