c0d17f6b3147dda7415acbf5d5f3023b9dc7bb66
Beatriz <beatriz.mediatto2@gmail.com>
2023-01-31 10:18:47 -0300

    quinto exercicio

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-1-NodeJs-runtime-assincrono/exercicios-do-dia/my-scripts/bmi.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-1-NodeJs-runtime-assincrono/exercicios-do-dia/my-scripts/bmi.js
@@ -15,6 +15,26 @@ function main(){
   const bmi = calculaBMI(weight, height);
 
   console.log(`BMI: ${bmi.toFixed(2)}`)
+
+  if(bmi < 18.5) {
+    console.log('Abaixo do peso(magreza)');
+    return ;
+  } if ( bmi >= 18.5 && bmi <= 24.9) {
+    console.log('Peso normal');
+    return;
+  } if(bmi >= 25 && bmi <= 29.9) {
+    console.log('Acima do peso(sobrepeso)');
+    return;
+  } if(bmi >= 30 && bmi <= 34.9) {
+    console.log('Obesidade grau 1');
+    return;
+  } if(bmi >= 35 && bmi <= 39.9) {
+    console.log('Obesidade grau 2');
+    return;
+  } if(bmi >= 40) {
+    console.log('Obesidade graus 3 e 4') 
+    return;
+  }
 }
 
 main()
