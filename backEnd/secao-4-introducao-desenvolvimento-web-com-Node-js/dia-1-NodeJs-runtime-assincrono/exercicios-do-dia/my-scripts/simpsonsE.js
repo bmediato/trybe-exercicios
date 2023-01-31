ba156781cf6bfd31fb059047b96f12a7f42ad0e5
Beatriz <beatriz.mediatto2@gmail.com>
2023-01-31 13:52:44 -0300

    exercicio 6.e e 6.f

---
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-1-NodeJs-runtime-assincrono/exercicios-do-dia/my-scripts/simpsonsE.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+
+async function addNelsonToFamily() {
+  const fileContent = await fs
+    .readFile('./simpsonsFamily.json', 'utf-8');
+
+  const simpsonsFamily = JSON.parse(fileContent);
+  simpsonsFamily.push({ id: '8', name: 'Nelson Muntz' });
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
+}
+
+function main() {
+  addNelsonToFamily();
+}
+main();
