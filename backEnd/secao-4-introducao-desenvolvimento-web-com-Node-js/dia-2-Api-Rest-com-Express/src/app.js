060eb2aad57170db3803aeb1ce61d05c4e4fb73a
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-01 08:23:21 -0300

    quarto exercicio

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-2-Api-Rest-com-Express/src/app.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-2-Api-Rest-com-Express/src/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+async function leitura() {
+  try {
+    const fileContent = await fs.readFile(moviesPath);
+    const movies = JSON.parse(fileContent);
+    console.log(movies);
+    return movies;
+  } catch (err) {
+    console.error(`Arquivo não pode ser lido: ${err}`);
+  }
+}
+
+leitura();
 
 module.exports = app;
