ab3a07055af6c3c725a54c7feb9a18cd21bcc7a5
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-01 09:33:54 -0300

    setimo, oitavo e nono requisito

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-2-Api-Rest-com-Express/src/app.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-2-Api-Rest-com-Express/src/app.js
@@ -37,4 +37,49 @@ app.get('/movies', async (req, res) => {
   }
 });
 
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await leitura();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await leitura();
+    const filmid = movies.findIndex((film) => film.id === +id);
+    movies[filmid] = { id: +id, movie, price };
+    const upDateMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, upDateMovies);
+    res.status(200).json(movies[filmid]);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await leitura();
+    const position = movies.filter((film) => film.id !== +id);
+    const updateMovies = JSON.stringify(position, null, 2);
+    await fs.writeFile(moviesPath, updateMovies);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 module.exports = app;
