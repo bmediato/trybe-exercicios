40819545d247ade7666d3f4400ec61789d153f37
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-02 10:44:31 -0300

    exercicio 1

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-3-testes-de-integração/cacau-trybe/tests/integration/chocolates.test.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-3-testes-de-integração/cacau-trybe/tests/integration/chocolates.test.js
@@ -115,4 +115,14 @@ describe('Testando a API Cacau Trybe', function () {
       ])
     })
   })
+  describe('Usando o método GET em /chocolates/total', function() {
+    it('Retorna a quatidade de tipos de chocolate que existem', async function() {
+      const response = await chai.request(app).get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal({
+        "totalChocolates": 4
+      })
+    })
+  })
 });
