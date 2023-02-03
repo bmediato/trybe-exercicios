6683e6f22d3422b5902f3f4134f15f286b83ba70
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-03 11:37:36 -0300

    cria novo endpoint, finaliza 10

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
@@ -5,6 +5,7 @@ const validationDescription = require('./middlewares/validationDescription');
 const validationCreatedAt = require('./middlewares/validationCreatedAt');
 const validationRating = require('./middlewares/validationRating');
 const validationDifficulty = require('./middlewares/validationDifficulty');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 app.use(express.json());
@@ -16,6 +17,15 @@ validationCreatedAt,
 validationDescription, 
 validationPrice, validationName, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+  const token = generateToken();
+  return res.status(200).json({ token });
 });
 
 module.exports = app;
