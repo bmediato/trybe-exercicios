3a5191f170a22975a9c3741885604a4356cc46f2
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-03 10:32:39 -0300

    cria middleware para validaçaõ do nome, finaliza exercicio 4

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const validationName = require('./middlewares/validationName');
 
 const app = express();
 app.use(express.json());
 
-app.post('/activities', (req, res) => {
+app.post('/activities', validationName, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
