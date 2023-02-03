d9d58c71ff202965cdd8999c003a2a54672f58ac
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-03 06:59:24 -0300

    refazendo

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/futbol-team/src/middlewares/validateTeam.js
+++
@@ -1,8 +0,0 @@
-const validateTeam = (req, res, next) => {
-  const { nome, sigla } = req.body;
-  if (!nome) return res.status(400).json({ message: 'O campo "nome" é obrigatorio' });
-  if (!sigla) return res.status(400).json({ message: 'O campo "sigla" é obrigatório' });
-  next();
-};
-
-module.exports = { validateTeam };
