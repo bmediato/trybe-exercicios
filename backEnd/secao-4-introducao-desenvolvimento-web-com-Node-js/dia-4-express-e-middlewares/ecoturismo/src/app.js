b61a9636baab471b20f2beda608295559a3bc297
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-03 10:41:52 -0300

    cria middleware para validação do preço, finaliza requisito 5

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const validationName = require('./middlewares/validationName');
+const validationPrice = require('./middlewares/validationPrice');
 
 const app = express();
 app.use(express.json());
 
-app.post('/activities', validationName, (req, res) => {
+app.post('/activities', validationName, validationPrice, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
