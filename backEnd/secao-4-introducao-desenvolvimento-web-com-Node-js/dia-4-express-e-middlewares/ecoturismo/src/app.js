00d174ed9dd998a0a3726233f0d4474473715f30
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-03 11:01:07 -0300

    cria middleware para validação da data, finaliza 7

--- backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
+++ backEnd/secao-4-introducao-desenvolvimento-web-com-Node-js/dia-4-express-e-middlewares/ecoturismo/src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const validationName = require('./middlewares/validationName');
 const validationPrice = require('./middlewares/validationPrice');
 const validationDescription = require('./middlewares/validationDescription');
+const validationCreatedAt = require('./middlewares/validationCreatedAt');
 
 const app = express();
 app.use(express.json());
 
-app.post('/activities', validationDescription, validationPrice, validationName, (req, res) => {
+app.post('/activities', 
+validationCreatedAt, validationDescription, validationPrice, validationName, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
