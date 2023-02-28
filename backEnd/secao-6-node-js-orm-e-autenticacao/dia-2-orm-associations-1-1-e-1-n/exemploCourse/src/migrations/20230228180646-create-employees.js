2d76d4dcc6dba0fee4727a6b20682d6d00c8f573
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-28 15:51:55 -0300

    atualizações

--- backEnd/secao-6-node-js-orm-e-autenticacao/dia-2-orm-associations-1-1-e-1-n/exemploCourse/src/migrations/20230228180646-create-employees.js
+++ backEnd/secao-6-node-js-orm-e-autenticacao/dia-2-orm-associations-1-1-e-1-n/exemploCourse/src/migrations/20230228180646-create-employees.js
@@ -2,20 +2,31 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'first_name',
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'last_name',
+      },
+      age: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('employees');
   }
 };
