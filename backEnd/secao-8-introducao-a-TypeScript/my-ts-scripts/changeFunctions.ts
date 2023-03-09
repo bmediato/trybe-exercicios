b45c4411e70e34cc2810608ed7f3ede5f7f1963f
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-09 19:10:34 -0300

    exercicio 1 - funcao 4

--- backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
+++ backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
@@ -35,3 +35,15 @@ const sortInput = (param: number[]) => {
 
 console.log(sortInput(numberList));
 
+// function 4 - Essa função é responsável por receber um objeto e formar uma frase utilizando as chaves do mesmo.
+
+const peopleInput = {
+  name: 'Rui',
+  age: 23,
+};
+
+const createSimpleSentence = (param: { name: string, age: number }) => {
+  return `Olá, meu nome é ${param.name} e tenho ${param.age} anos.`;
+}
+
+console.log(createSimpleSentence(peopleInput));
