9e3de2df90464cd220ba12778f9577006ae72179
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-09 19:06:07 -0300

    exercicio 1 - funcao 3

--- backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
+++ backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
@@ -23,3 +23,15 @@ const validateEmailFormat = (param: string) => {
 };
 
 console.log(validateEmailFormat(emailInput));
+
+
+// function 3 - Essa função recebe uma lista e ordena seu conteúdo de forma crescente.
+
+const numberList = [10, 5, 18, 2, 8, 23];
+
+const sortInput = (param: number[]) => {
+  return param.sort(function (a, b) { return a - b });
+};
+
+console.log(sortInput(numberList));
+
