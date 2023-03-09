5dd2227985c9a5e8247fd82c197f488358ebc4ec
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-09 19:02:50 -0300

    exercicio 1 - funcao 1

---
+++ backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
@@ -0,0 +1,14 @@
+// function 1 
+const numberInput = 7;
+
+const isItAPrime = (param: number): boolean => {
+  for (let i = 2; i < param; i++)
+    if (param % i === 0) {
+      return false;
+    }
+  return param > 1;
+};
+
+isItAPrime(numberInput)
+  ? console.log(`${numberInput} é primo`)
+  : console.log(`${numberInput} não é primo`);
