1aa606c8cdaac21e46e1f543a363ae39e131c4ec
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-09 19:12:46 -0300

    exercicio 1 - funcao 5

--- backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
+++ backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/changeFunctions.ts
@@ -47,3 +47,15 @@ const createSimpleSentence = (param: { name: string, age: number }) => {
 }
 
 console.log(createSimpleSentence(peopleInput));
+
+// function 5 - Essa é uma função que verifica se a idade passada é maior ou menor de 18 anos.
+
+const ageInput = 15;
+
+const isOfLegalAge = (param: string | boolean) => {
+  return !!param
+}
+
+ageInput >= 18
+  ? console.log(isOfLegalAge('true'))
+  : console.log(isOfLegalAge(false));
