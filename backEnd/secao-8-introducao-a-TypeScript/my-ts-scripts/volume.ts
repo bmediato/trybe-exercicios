e9fe4ef864fb47a58d48c57f65e0c4343ee6684d
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-10 09:30:07 -0300

    exercicio 6

---
+++ backEnd/secao-8-introducao-a-TypeScript/my-ts-scripts/volume.ts
@@ -0,0 +1,9 @@
+const units4 = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
+
+function convert(valor: number, unidadeBase: string, unidadeConversao: string): number {
+  const fromIndex = units4.indexOf(unidadeBase);
+  const toIndex = units4.indexOf(unidadeConversao);
+  const expoente = toIndex - fromIndex;
+
+  return valor * Math.pow(1000, expoente);
+}
