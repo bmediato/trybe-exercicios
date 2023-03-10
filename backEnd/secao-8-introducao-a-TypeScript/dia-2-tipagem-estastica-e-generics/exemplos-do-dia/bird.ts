f8300cb18148b6cd5572cdffc39be2f64d8ad550
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-10 11:10:41 -0300

    type aliases

---
+++ backEnd/secao-8-introducao-a-TypeScript/dia-2-tipagem-estastica-e-generics/exemplos-do-dia/bird.ts
@@ -0,0 +1,18 @@
+type obj = {
+  bird: string;
+}
+
+type obj1 = {
+  x: number;
+  y: number;
+}
+
+function sum(value: obj1): number {
+  return value.x + value.y;
+}
+
+type addres = {
+  rua: string;
+  numero: number;
+
+}
