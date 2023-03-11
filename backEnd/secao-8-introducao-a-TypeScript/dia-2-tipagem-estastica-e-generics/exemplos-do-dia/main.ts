eb7cd6ec11673c984929bc00dc1e9b1b987286fe
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-11 09:20:40 -0300

    modificações

---
+++ backEnd/secao-8-introducao-a-TypeScript/dia-2-tipagem-estastica-e-generics/exemplos-do-dia/main.ts
@@ -0,0 +1,11 @@
+// ./main.ts
+
+import connection from './models/connection';
+
+const main = async () => {
+  const result = await connection.execute('SELECT * FROM books');
+  const [rows] = result;
+  console.log(rows);
+};
+
+main();
