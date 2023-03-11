50f30bff017cd94ec3702520f539bec36e7ff5aa
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-11 09:33:03 -0300

    implementando metodo create

--- backEnd/secao-8-introducao-a-TypeScript/dia-2-tipagem-estastica-e-generics/exemplos-do-dia/models/Books.ts
+++ backEnd/secao-8-introducao-a-TypeScript/dia-2-tipagem-estastica-e-generics/exemplos-do-dia/models/Books.ts
@@ -1,7 +1,15 @@
 // ./models/Book.ts
 
 import connection from './connection';
-import { Pool } from 'mysql2/promise';
+import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
+
+export interface Book {
+  id?: number;
+  title: string;
+  price: number;
+  author: string;
+  isbn: string;
+}
 
 export default class BookModel {
   private connection: Pool;
@@ -10,9 +18,14 @@ export default class BookModel {
     this.connection = connection;
   }
 
-  public async getAll() {
-    const result = await this.connection.execute('SELECT * FROM books');
-    const [rows] = result;
-    return rows;
+  public async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
+      [title, price, author, isbn]
+    );
+
+    return { id: insertId, ...book };
   }
 }
