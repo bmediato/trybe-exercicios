cd4f5533b1af4905c8efb04e63925fb28177e471
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-13 09:44:16 -0300

    exercicio 4

--- backend/secao-8-introducao-a-TypeScipt/dia-2-tipagem-estastica-e-generics/exercicios-do-dia/agora-pratica/index.ts
+++ backend/secao-8-introducao-a-TypeScipt/dia-2-tipagem-estastica-e-generics/exercicios-do-dia/agora-pratica/index.ts
@@ -1,6 +1,6 @@
 // classe Car
 import Car from './car';
-import Pizza from './pizza';
+import { Pizza, PizzaComum, PizzaSugar, PizzaVegetarian } from './pizza';
 
 const gol = new Car("Volkswagen", "prata", 4);
 
@@ -36,12 +36,48 @@ const calabresa: Pizza = {
   slices: 8
 }
 
+console.log(calabresa);
+
 const marguerita: Pizza = {
   flavor: 'Marguerita',
   slices: 6
 }
 
+console.log(marguerita);
+
 const nutella: Pizza = {
   flavor: 'Nutella',
+  slices: 4
+}
+
+console.log(nutella);
+
+const commun1: PizzaComum = {
+  flavor: 'Frango',
+  slices: 8
+}
+
+const commum2: PizzaComum = {
+  flavor: 'Calabresa',
+  slices: 4,
+}
+
+const commum33: PizzaComum = {
+  flavor: 'Pepperoni',
+  slices: 6,
+}
+
+const vegetarian1: PizzaVegetarian = {
+  flavor: 'Cogumelos',
+  slices: 8,
+}
+
+const vegetarian2: PizzaVegetarian = {
+  flavor: 'Palmito',
+  slices: 6
+}
+
+const doce: PizzaSugar = {
+  flavor: 'Goiabada com Queijo',
   slices: 4
 }
