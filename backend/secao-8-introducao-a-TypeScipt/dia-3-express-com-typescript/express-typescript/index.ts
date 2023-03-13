6c04484416a3de91d2beacbaaae5e3f43f4edffc
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-13 14:57:51 -0300

    setup configurado - pronto para começar o CRUD

--- backend/secao-8-introducao-a-TypeScipt/dia-3-express-com-typescript/express-typescript/index.ts
+++ backend/secao-8-introducao-a-TypeScipt/dia-3-express-com-typescript/express-typescript/index.ts
@@ -1,7 +1,7 @@
 // ./index.ts
-
-import express from 'express';
 import statusCodes from './statusCodes';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 const app = express();
 
@@ -9,9 +9,33 @@ app.use(express.json());
 
 const PORT = 8000;
 
-// A utilização do underline antes de um parâmetro é uma boa prática quando não estamos fazendo o uso do mesmo.
-app.get('/', (_req, res) => {
-  res.status(statusCodes.OK).send('Express + TypeScript');
+app.get('/', (_req: Request, res: Response) => {
+  res.status(statusCodes.OK).send('Express + TypeScript')
+});
+
+app.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'BadRequestError':
+      res.status(400).json({ message });
+      break;
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
 });
 
 app.listen(PORT, () => {
