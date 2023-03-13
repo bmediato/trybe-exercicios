05f2c6da336f6138cb87e077363019cce460e424
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-13 10:43:20 -0300

    quinto exercicio concluido

--- backend/secao-8-introducao-a-TypeScipt/dia-2-tipagem-estastica-e-generics/exercicios-do-dia/sequelize-com-typescript/src/index.ts
+++ backend/secao-8-introducao-a-TypeScipt/dia-2-tipagem-estastica-e-generics/exercicios-do-dia/sequelize-com-typescript/src/index.ts
@@ -1,8 +1,17 @@
 import Author from './database/models/authorModel';
+import Book from './database/models/bookModel';
 
 (async () => {
   const authors = await Author.findAll();
   console.table(authors.map((author) => author.toJSON()));
+
+  process.exit(0);
+})();
+
+
+(async () => {
+  const books = await Book.findAll();
+  console.table(books.map((book) => book.toJSON()));
 
   process.exit(0);
 })();
