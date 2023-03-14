12379726c53bbf82196ee435166a8a3cf2ae247e
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-14 07:53:57 -0300

    modificações

--- backend/secao-8-introducao-a-TypeScipt/dia-3-express-com-typescript/express-typescript/routes/books.routes.ts
+++ backend/secao-8-introducao-a-TypeScipt/dia-3-express-com-typescript/express-typescript/routes/books.routes.ts
@@ -2,15 +2,17 @@
 
 import { Router } from 'express';
 import BooksController from '../controllers/books.controller';
-import validationBook from '../middlewares/books.middleware';
 
 const router = Router();
 
 const booksController = new BooksController();
 
+const booksSlashId = '/books/:id';
+
 router.get('/books', booksController.getAll);
-router.get('/books/:id', booksController.getById);
+router.get(booksSlashId, booksController.getById);
 router.post('/books', booksController.create);
-router.put('/books/:id', booksController.update);
+router.put(booksSlashId, booksController.update);
+router.delete(booksSlashId, booksController.remove);
 
 export default router; 
