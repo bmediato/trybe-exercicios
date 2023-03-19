c0604b3136cd48530df8b9a7e1631142d066121c
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-19 11:50:10 -0300

    feat: add exercicio de fixação

--- backEnd/secao-9-POO-e-Solid/dia-1-introducao-a-orientacao-a-objetos/exemplos-do-dia/tvClass.ts
+++ backEnd/secao-9-POO-e-Solid/dia-1-introducao-a-orientacao-a-objetos/exemplos-do-dia/tvClass.ts
@@ -1,21 +1,36 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectTo?: string;
 
   constructor(brand: string, size: number, resolution: string, connections: string[]) {
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
   turnOn() {
-    console.log(`brand: ${this.brand}, size: ${this.size}, resolution: ${this.resolution}, connections: ${this.connections}`)
+    console.log(`brand: ${this._brand}, size: ${this._size}, resolution: ${this._resolution}, connections: ${this._connections}`)
+  }
+
+  get connectTo(): string | undefined {
+    return this._connectTo;
+  }
+
+  set connectTo(value: string | undefined) {
+    if (!value || this._connections.includes(value)) {
+      this._connectTo = value;
+      console.log(this.connectTo);
+    } else {
+      console.log('Sorry, connection unavailable');
+    }
   }
 }
 
 const tv = new Tv('lg', 32, '4K', ['HDMI', 'Wifi', 'Ethernet'],)
 tv.turnOn();
+tv.connectTo = 'Wifi';
+console.log('connected to:', tv.connectTo);
