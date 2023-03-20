f2526f77f1a58b850e84d0651a409dd6116b2b72
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-20 20:37:36 -0300

    feat: finaliza 2

--- backEnd/secao-9-POO-e-Solid/dia-2-heranca-e-composicao/exercicios-do-dia/index.ts
+++ backEnd/secao-9-POO-e-Solid/dia-2-heranca-e-composicao/exercicios-do-dia/index.ts
@@ -1,64 +1,15 @@
-// `Class`: Person
-// `Attributes`:
-//     - name: nome da pessoa
-//     - birthDate: data de nascimento da pessoa
-// `Methods`:
-//     - Getters/Setters
-//     - constructor: deve receber como parâmetro nome e data de nascimento
-// `Validations`:
-//     - O nome deve ter no mínimo três caracteres
-//     - A data de nascimento não pode ser uma data no futuro
-//     - A pessoa não pode possuir mais de 120 anos
+// Para testar!
+// ./index.ts
 
-class Person {
-  private _name: string;
-  private _birthDate: Date;
+import Person from './person';
 
-  constructor(name: string, birthDate: Date) {
-    this._name = name;
-    this._birthDate = birthDate;
-  }
+const maria = new Person('Maria da Consolação', new Date('1980/01/25'));
+const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
 
-  get name(): string {
-    return this._name;
-  }
+console.log(maria);
+console.log(luiza);
 
-  set name(value: string) {
-    this.validateName(value);
-    this._name = value;
-  }
-
-  private validateName(name: string): void {
-    if (name.length < 3) {
-      throw new Error("o nome deve ter pelo menos 3 letras");
-    }
-  }
-
-  get birthDate(): Date {
-    return this._birthDate;
-  }
-
-  set birthDate(value: Date) {
-    this.validateBirthDate(value);
-    this._birthDate = value;
-  }
-
-  private validateBirthDate(birthDate: Date): void {
-    const today = new Date();
-    const birthDateInM = birthDate.getTime();
-    const todayInM = today.getTime();
-
-    if (birthDateInM > todayInM) {
-      throw new Error("data de nascimento invalida");
-    }
-    if (this.getAge(birthDate) > 120) {
-      throw new Error('A pessoa não pode ter mais de 120 anos');
-    }
-  }
-
-  private getAge(date: Date): number {
-    const ageInMs: number = new Date().getTime() - date.getTime();
-    const oneYearInMs: number = 31536000000;
-    return Math.floor(ageInMs / oneYearInMs);
-  }
-}
+// deve retornar erro
+// const invalidPersonName = new Person('An', new Date('2000/06/07'));
+// deve retornar erro
+// const invalidPersonAge = new Person('Ana Vitória', new Date('1900/06/07'));
