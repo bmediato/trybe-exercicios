4553cd99f53adf2bec63061e2a542d9cb30afbb9
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-20 20:40:08 -0300

    feat: finaliza 3

---
+++ backEnd/secao-9-POO-e-Solid/dia-2-heranca-e-composicao/exercicios-do-dia/personFun.ts
@@ -0,0 +1,15 @@
+// `Interface`: Employee
+// `Attributes`:
+//     - registration: número do registro
+//     - salary: valor do salário
+//     - admissionDate: data de admissão
+// `Methods`:
+//     - generateRegistration: retorna uma string única gerada como registro
+
+export default interface Employee {
+  registration: string;
+  salary: number;
+  admissionDate: Date;
+
+  generateRegistration(): string;
+}
