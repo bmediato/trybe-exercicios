163688ae6ae947fa3ad18063a6f2841a7e1b12ed
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-21 15:11:33 -0300

    feat: cria classe meleecharacter q estenda a character

--- backEnd/secao-9-POO-e-Solid/dia-3-polimorfismo/exemplos-do-dia/superSmashBros.ts
+++ backEnd/secao-9-POO-e-Solid/dia-3-polimorfismo/exemplos-do-dia/superSmashBros.ts
@@ -2,3 +2,13 @@ abstract class Character {
   abstract talk(): void;
   abstract specialMode(): void;
 }
+
+class MeleeCharacter extends Character {
+  talk(): void {
+    console.log('Olá kiki');
+  }
+
+  specialMode(): void {
+    console.log('Voando');
+  }
+}
