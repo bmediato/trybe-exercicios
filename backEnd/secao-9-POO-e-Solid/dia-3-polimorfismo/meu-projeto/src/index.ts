2988c70467a97fd1c206ca3732f2c298e57e54f4
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-22 07:13:02 -0300

    sexto exercicio concluido

--- backEnd/secao-9-POO-e-Solid/dia-3-polimorfismo/meu-projeto/src/index.ts
+++ backEnd/secao-9-POO-e-Solid/dia-3-polimorfismo/meu-projeto/src/index.ts
@@ -1,9 +1,15 @@
 import Clube from "./Clube";
 import QuadraFutebol from "./QuadraFutebol";
+import QuadraTenis from "./QuadraTenis";
 
 const club = new Clube();
 const quadraFutebol = new QuadraFutebol();
+const quadraTenis = new QuadraTenis();
 
 club.adicionarQuadra(quadraFutebol);
 const reserverQuadraFutebol = club.buscarQuadra<QuadraFutebol>(0).reservar(new Date('2023-03-23'));
 console.log(reserverQuadraFutebol);
+
+club.adicionarQuadra(quadraTenis);
+const reservarQuadraTenis = club.buscarQuadra<QuadraTenis>(1).reservar(new Date('2023-03-21'));
+console.log(reservarQuadraTenis);
