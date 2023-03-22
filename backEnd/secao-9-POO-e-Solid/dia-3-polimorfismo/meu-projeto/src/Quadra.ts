f1509e3375626c5d9974928b057fee0967a50dcf
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-22 06:47:57 -0300

    segundo exercicio concluido

--- backEnd/secao-9-POO-e-Solid/dia-3-polimorfismo/meu-projeto/src/Quadra.ts
+++ backEnd/secao-9-POO-e-Solid/dia-3-polimorfismo/meu-projeto/src/Quadra.ts
@@ -1,4 +1,8 @@
 // src/Quadra.ts
-class Quadra { }
+import { IAgenda } from "./interfaces/IAgenda";
+
+abstract class Quadra {
+  abstract reservar<T>(data: Date): IAgenda<T>;
+}
 
 export default Quadra;
