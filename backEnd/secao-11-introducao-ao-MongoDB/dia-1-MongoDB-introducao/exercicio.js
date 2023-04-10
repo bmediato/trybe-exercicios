a531ffd0c58e67812dc91d4bd18b2f05d981e50a
Beatriz <beatriz.mediatto2@gmail.com>
2023-04-10 19:35:07 -0300

    quarto exercicio

--- backEnd/secao-11-introducao-ao-MongoDB/dia-1-MongoDB-introducao/exercicio.js
+++ backEnd/secao-11-introducao-ao-MongoDB/dia-1-MongoDB-introducao/exercicio.js
@@ -8,3 +8,6 @@ db.bios.findOne({_id: 8}, {_id:1, name:1});
 
 // terceiro exercicio 
 db.bios.findOne({_id: 8}, {_id: 0,birth:1, name:1});
+
+// quarto exercicio 
+db.bios.find({'name.first': 'John'}).pretty();
