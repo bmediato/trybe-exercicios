bbda19c1969609a4f69e6aaa676264122819c594
Beatriz <beatriz.mediatto2@gmail.com>
2023-04-11 17:05:14 -0300

    desafios 7,8 e 9 da aula ao vivo

--- backEnd/secao-11-introducao-ao-MongoDB/dia-1-MongoDB-introducao/queries.mongodb.js
+++ backEnd/secao-11-introducao-ao-MongoDB/dia-1-MongoDB-introducao/queries.mongodb.js
@@ -1,7 +1,28 @@
 use('movies');
 
+// 1
 // db.filmes.find({ano: {$lt: 2000}}).pretty();
 
+// 2
 // db.filmes.find({ano: {$lte: 2000}}).pretty();
 
-db.filmes.find({ano: {$gt: 1993}}).pretty();
+// 3
+// db.filmes.find({ano: {$gt: 1993}}).pretty();
+
+// 4
+// db.filmes.find({ano: {$gte: 1993}}).pretty();
+
+//5 
+// db.filmes.find({ano: {$gte:1993, $lte: 2000}});
+
+// 6 
+// db.filmes.find({ano: 1993});
+
+// 7 
+// db.filmes.find({'avaliacao.bom': {$ne: 7}});
+
+// 8 
+// db.filmes.find({ano: {$in: [1995, 1968, 2001]}});
+
+// 9 
+db.filmes.find({ano: {$nin: [2013, 2008]}});
