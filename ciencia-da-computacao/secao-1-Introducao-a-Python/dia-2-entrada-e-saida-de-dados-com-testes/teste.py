8314edf46f966c83a165ec30a22a9bec6db693c3
Beatriz <beatriz.mediatto2@gmail.com>
2023-05-30 09:42:09 -0300

    feat: exemplo do dia

--- ciencia-da-computacao/secao-1-Introducao-a-Python/dia-2-entrada-e-saida-de-dados-com-testes/teste.py
+++ ciencia-da-computacao/secao-1-Introducao-a-Python/dia-2-entrada-e-saida-de-dados-com-testes/teste.py
@@ -1,8 +1,17 @@
+# 1
 # import sys
 
 # if __name__ == "__main__":
 #     for argument in sys.argv:
 #         print("Received ->", argument)
 
-user_input = input("Digite uma mensagem:")
-print("O valor recebido foi:", user_input)
+# 2
+# user_input = input("Digite uma mensagem:")
+# print("O valor recebido foi:", user_input)
+
+# 3
+import sys
+
+
+err = "Arquivo não encontrado"
+print(f"Erro aconteceu: {err}", file=sys.stderr)
