6a0ebee1d315a57fec92c2875fa365330fe242a4
Beatriz <beatriz.mediatto2@gmail.com>
2023-05-30 10:31:12 -0300

    feat: exercicio de fixação 2

--- ciencia-da-computacao/secao-1-Introducao-a-Python/dia-2-entrada-e-saida-de-dados-com-testes/exemplos_dia.py
+++ ciencia-da-computacao/secao-1-Introducao-a-Python/dia-2-entrada-e-saida-de-dados-com-testes/exemplos_dia.py
@@ -1,4 +1,20 @@
-nome = input("Informe seu nome:")
+#  1
+# nome = input("Informe seu nome:")
 
-for letter in nome:
-    print(letter)
+# for letter in nome:
+#     print(letter)
+
+
+number = input("Insira valores aqui, separados por espaço:")
+
+number_arr = number.split()
+
+sum = 0
+
+for num in number_arr:
+    if not num.isdigit():
+        print(f"Error ao somar valores, {num} não é um dígito")
+    else:
+        sum += int(num)
+
+print(f"A soma dos valores válidos é: {sum}")
